0f5789c74cc4df56ce89bd659b0b44e8892d5366
johnfn <johnfn@gmail.com>
2010-08-28 13:49:49 -0700

    First multiplayer commit: you can see other people!

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,6 @@ var canv, context;
 var W     = 10; //Width of tiles
 var walls = "#"; //Can't walk on these
 
-var state = {};
 
 // This is actually faster.
 var sqrt = Math.sqrt,
@@ -50,6 +49,7 @@ var map = [ ".........................",
  */
 
 var Constants = {
+    port : 80,
     tileSize : W,
     tilesAcross : 25,
     tilesUp : 25,
@@ -66,6 +66,8 @@ var gameState = {
     bullets : [],
     monsters : [],
     socket : undefined,
+    newState : {},  //This is what we send to the server every loop
+    players : [],
 };
 
 /*
@@ -90,8 +92,26 @@ var utils = {
             if (hyp === 0)
                 return [0, 0];
             return [x/hyp, y/hyp];
-        }
+        },
+    send :
+        function (msg) { 
+            gameState.socket.send(msg);     
+        },
+    /*
+     * Finds an object with specified ID in list list. Returns false if
+     * no such object exists.
+     */
+    findObjectWithID : 
+        function (list, ID){
+            for (var i=0;i<list.length;i++){
+                if (list[i].ID == ID){
+                    return list[i];
+                }
+            }
+            return false;
+        },
 };
+
 
 /*
  * Dynamic objects
@@ -110,6 +130,31 @@ function Point(x, y) {
 }
 
 /*
+ *
+ * Player
+ *
+ * Distinguished from curPlayer; these are non playable characters, controlled by other people.
+ */
+
+function Player(x, y, ID, color){
+    this.x=x;
+    this.y=y;
+    this.ID=ID;
+    this.color=color;
+}
+Player.prototype = {
+    move : function(x, y){
+        this.x=x;
+        this.y=y;
+    },
+    draw : function() { 
+        context.fillStyle = this.color;
+        context.fillRect(this.x*W, this.y*W, W, W);
+    },
+};
+
+
+/*
  * Bullet
  *
  * Initialize with x and y and optional dx, dy, speed.
@@ -125,7 +170,7 @@ function Bullet(x, y, color, speed, creator, dx, dy) {
     this.point = new Point(x,y);
     this.color = color;
     this.creator = creator;
-    this.id = getUniqueID();
+    this.ID = getUniqueID();
     this.init = function() {
         if (dx != undefined) {
             this.dx=dx;
@@ -224,7 +269,7 @@ Bullet.prototype = {
     destroy :
         function() {
             for (x in gameState.bullets) { 
-                if (gameState.bullets[x].id == this.id) { 
+                if (gameState.bullets[x].ID == this.ID) { 
                     gameState.bullets.splice(x, 1);
                     break;
                 }
@@ -242,7 +287,7 @@ function Monster(x, y, color) {
     this.x = x*Constants.tileSize;
     this.y = y*Constants.tileSize;
     this.color = color;
-    this.id = getUniqueID();
+    this.ID = getUniqueID();
     this.rect = new Rect(this.x, this.y, this.x + this.W, this.y + this.W);
     this.init();
 }
@@ -297,7 +342,7 @@ Monster.prototype = {
     destroy :
         function() {
             for (x in gameState.monsters) { 
-                if (gameState.monsters[x].id == this.id) { 
+                if (gameState.monsters[x].ID == this.ID) { 
                     gameState.monsters.splice(x, 1)
                     break;
                 }
@@ -346,7 +391,7 @@ Monster.prototype = {
  */
 var curPlayer = {
     x : 10,
-    id : getUniqueID(),
+    ID : getUniqueID(),
     y : 10,
     HP : 10,
     maxHP : 10,
@@ -360,7 +405,10 @@ var curPlayer = {
         if (!this.checkCollisions(dx, dy)) {
             this.x += dx;
             this.y += dy;
+            gameState.newState.x = curPlayer.x;
+            gameState.newState.y = curPlayer.y;
         }
+
         this.rect = new Rect(this.x*W, this.y*W, this.x*W + W, this.y*W + W);
     },
     draw : function() { 
@@ -389,10 +437,14 @@ function draw() {
     drawMap();
     drawBullets();
     drawMonsters();
+
     curPlayer.draw();
-    /*
     drawOtherCharacters();
-    */
+}
+function drawOtherCharacters(){
+    for (c in gameState.players) {
+        gameState.players[c].draw();
+    }
 }
 
 function drawBullets() {
@@ -466,6 +518,22 @@ function getUpdatesFromServer() {
     }
 }
 
+function serverUpdate(json){
+    var update = JSON.parse(json);
+    if (update.ID != curPlayer.ID){
+        var obj = utils.findObjectWithID(gameState.players, update.ID);
+        if (!obj){
+            //Not found, so add him. New player! TODO more fanfare
+            gameState.players.push(new Player(update.x, update.y, update.ID, "#ff5555"));
+        } else {
+            obj.x = update.x;
+            obj.y = update.y;
+        }
+        
+    }
+    //console.log(obj);
+}
+
 /* 
  * Send information to the server like so.
  *
@@ -476,11 +544,12 @@ function getUpdatesFromServer() {
  *  dropped   :    "        "
  */ 
 function sendUpdatesToServer() {
-
+    utils.send(JSON.stringify(gameState.newState));
 }
 /* Handle all game actions. This is called several times a second */
 function gameLoop() {
-    state = {};
+    gameState.newState = {};
+    gameState.newState.ID = curPlayer.ID;
     getUpdatesFromServer();
     updateLocal();
     draw();
@@ -490,21 +559,16 @@ function gameLoop() {
 function initialize() {
     io.setPath('/client/');
     gameState.socket = new io.Socket(null, { 
-        port: 80,
+        port: Constants.port,
         transports: ['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling']
     });
     gameState.socket.connect();
     
-    gameState.socket.on('message', function(data) {
-        $('#reciever').append('<li>' + data + '</li>');
-    });
+    gameState.socket.on('message', serverUpdate);
     
    //$('#sender').bind('click', function() {
    //});
    
-   gameState.socket.on('message', function(data){
-     $('#reciever').append('<li>' + data + '</li>');  
-   });
 
     var m = new Monster(5, 5, "ff5555");
     for (var i=0;i<255;i++) {
@@ -528,7 +592,6 @@ $(function() {
         gameState.mouseX = e.clientX;
         gameState.mouseY = e.clientY;
     }).mousedown(function(e) {
-        gameState.socket.send("Fire bullet!");     
         gameState.mouseDown = true;
     }).mouseup(function(e) {
         gameState.mouseDown = false;
