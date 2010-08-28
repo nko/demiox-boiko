eebda4480290beecdae197971ecd2c543016cce7
johnfn <johnfn@gmail.com>
2010-08-27 20:40:13 -0700

    Bullet shooting is a go!

--- main.js
+++ main.js
@@ -2,13 +2,10 @@ $(function(){
     //Globals
     var W=10; //Width of tiles
     var walls = "#"; //Can't walk on these
-    var keys = new Array(255);
 
-    var mouseX=0, mouseY=0;
     var state = {};
     var canv = document.getElementById('main');
     var context = canv.getContext('2d');
-
 
     var map = [ ".........................",
                 ".........................",
@@ -42,25 +39,24 @@ $(function(){
                 ".........................", ];
 
 
-
-    $(document).keydown(function(e){ 
-        console.log(e.which);
-        keys[e.which] = true;
-    });
-    $(document).keyup(function(e){ 
-        keys[e.which] = false;
-    });
-    $(document).mousemove(function(e){
-        mouseX = e.clientX;
-        mouseY = e.clientY;
-    });
-    $(document).mousedown(function(e){
-
-    });
-
     /*
      * Static objects
      */
+
+    var Constants = {
+        tilesAcross : 25,
+        tilesUp : 25,
+        widthPX : 25 * W,
+        heightPX : 25 * W,
+    };
+
+    var gameState = {
+        keys : new Array(255),
+        mouseX : 0,
+        mouseY : 0,
+        mouseDown : false,
+        bullets : [],
+    };
 
     var curPlayer = {
         x : 10,
@@ -69,6 +65,25 @@ $(function(){
         maxHP : 10,
     };
     
+    /*
+     * Handlers
+     */
+
+    $(document).keydown(function(e){ 
+        console.log(e.which);
+        gameState.keys[e.which] = true;
+    });
+    $(document).keyup(function(e){ 
+        gameState.keys[e.which] = false;
+    });
+    $(document).mousemove(function(e){
+        gameState.mouseX = e.clientX;
+        gameState.mouseY = e.clientY;
+    });
+    $(document).mousedown(function(e){
+        gameState.mouseDown = false;
+    });
+
     /*
      * Dynamic objects
      */
@@ -83,31 +98,60 @@ $(function(){
      *
      * dx/dy should be normalized (squared they should add to 1).
      */
-    function Bullet(x, y, dx, dy, speed){
+    function Bullet(x, y, speed, dx, dy){
         this.x = x;
         this.y = y;
-        if (dx != undefined){
-            this.dx=dx;
-            this.dy=dy;
-            this.speed=speed;
-        } else {
-            var dxfar = this.x - mouseX;
-            var dyfar = this.y - mouseY;
+        this.W = 4;
+        this.id = getUniqueID();
+        this.init = function(){ 
+            if (dx != undefined){
+                this.dx=dx;
+                this.dy=dy;
+                this.speed=speed;
+            } else {
+                var dxfar = gameState.mouseX - this.x;
+                var dyfar = gameState.mouseY - this.y;
 
-            if (dxfar == 0) dxfar = .1;
-            if (dyfar == 0) dyfar = .1; //Prevent div by 0 or other awkward things from happening
+                if (dxfar == 0) dxfar = .1;
+                if (dyfar == 0) dyfar = .1; //Prevent div by 0 or other awkward things from happening
 
-            var hyp = Math.sqrt(dxfar * dxfar + dyfar * dyfar);
+                var hyp = Math.sqrt(dxfar * dxfar + dyfar * dyfar);
 
-            this.dx = this.dxfar / hyp;
-            this.dy = this.dyfar / hyp;
+                this.dx = (dxfar / hyp)*speed;
+                this.dy = (dyfar / hyp)*speed;
+            }
+            gameState.bullets.push(this);
         }
 
         this.update = function(){
+            var destroy = false;
             this.x += this.dx;
             this.y += this.dy;
+            //Out of bounds?
+            if (this.x > Constants.widthPX || this.x < 0 || this.y > Constants.heightPX || this.y < 0){
+                destroy = true;
+            }
+
+            //TODO check collisions
+
+
+
+            if (destroy){
+                this.destroy();
+            }
         }
 
+        this.destroy = function(){
+            for (x in gameState.bullets) { 
+                if (gameState.bullets[x].id == this.id) { 
+
+                    gameState.bullets.splice(x, 1)
+                    break;
+                }
+            }
+        }
+
+        this.init();
 
     }
 
@@ -118,17 +162,35 @@ $(function(){
      */
     function draw(){
         drawMap();
+        drawBullets();
         drawCurPlayer();
         /*
         drawOtherCharacters();
         */
     }
+
+    function drawBullets(){
+        for (b in gameState.bullets){
+            var cBul = gameState.bullets[b];
+            context.fillStyle = "#000000";
+            context.fillRect(cBul.x-cBul.W/2, cBul.y-cBul.W/2, cBul.W, cBul.W);
+            cBul.update();
+        }
+    }
+
     function movePlayer(){
-        curPlayer.y += keys[83] - keys[87];
-        curPlayer.x += keys[68] - keys[65];
+        curPlayer.y += gameState.keys[83] - gameState.keys[87];
+        curPlayer.x += gameState.keys[68] - gameState.keys[65];
+        //TODO collision detection =)
+
+    }
+    function shootBullet(){
+        var b = new Bullet(curPlayer.x*W, curPlayer.y*W, 12);
+        console.log(gameState.bullets.length);
     }
     function updateLocal(){
         movePlayer();
+        shootBullet();
     }
 
     function drawCurPlayer(){
@@ -136,8 +198,8 @@ $(function(){
         context.fillRect(curPlayer.x*W, curPlayer.y*W, W, W);
     }
     function drawMap(){
-        for (var i=0;i<25;i++){
-            for (var j=0;j<25;j++){
+        for (var i=0;i<Constants.tilesAcross;i++){
+            for (var j=0;j<Constants.tilesUp;j++){
                 if (map[i][j] == ".")
                     context.fillStyle = "#55ff55";
                 else
@@ -145,7 +207,22 @@ $(function(){
                 context.fillRect(i*W,j*W,W,W);
             }
         }
+    }
 
+
+
+    /*
+     * This is a puzzler.
+     *
+     * Eventually, could replace this with asking the server. 
+     *
+     * Even better solution: the server allocates IDs to each 
+     * client ahead of time, and we just use those IDs and flag 
+     * when we're nearly done.
+     *
+     */
+    function getUniqueID(){
+        return ~~(Math.random()*1e20); //Mathematically sound
     }
 
     /*
@@ -185,7 +262,7 @@ $(function(){
 
     function initialize(){
         for (var i=0;i<255;i++){
-            keys[i]=false;
+            gameState.keys[i]=false;
         }
         setInterval(gameLoop, 100);
     }
